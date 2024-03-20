557f1cffee77236c519d955bd9198f5efb71f36e
CatrinRees <kieran_sears@hotmail.com>
2024-03-20 16:34:02 +0000

    removing comments

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,6 @@ mod config;
 
 fn main() {
 
-    // load config for model path etc.
-
     match config::load_config() {
         Ok(app_config) => {
             let model_table =  app_config.get_table("model").unwrap();
@@ -20,37 +18,25 @@ fn main() {
             let folder_path: String = model_table.get("path").unwrap().to_string();
             println!("Folder path: {}", folder_path);
 
-            // load a GGML model from disk
             let llama = load::<Llama>(
-                // path to GGML file
                 std::path::Path::new(&folder_path),
-                // llm::ModelParameters
                 Default::default(),
-                // load progress callback
                 load_progress_callback_stdout,
             )
             .unwrap_or_else(|err| panic!("Failed to load model: {err}"));
 
-            // use the model to generate text from a prompt
             let mut session = llama.start_session(Default::default());
             let res = session.infer::<std::convert::Infallible>(
-                // model to use for text generation
                 &llama,
-                // randomness provider
                 &mut rand::thread_rng(),
-                // the prompt to use for text generation, as well as other
-                // inference parameters
                 &llm::InferenceRequest {
                     prompt: "Rust is a cool programming language because",
                     ..Default::default()
                 },
-                // llm::OutputRequest
                 &mut Default::default(),
-                // output callback
                 |t| {
                     print!("{t}");
                     std::io::stdout().flush().unwrap();
-
                     Ok(())
                 },
             );
